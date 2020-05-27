7d1740b424a4f2782641c35dac45aad6c6e41436
Grzegorz Sadowski <sadowskigp@gmail.com>
2020-05-27 13:56:57 +0200

    [WIP][PoC] Customizing Checkout flow

---
+++ src/Action/ResolveNextRouteAction.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Action;
+
+use Payum\Core\Action\ActionInterface;
+use Sylius\Bundle\PayumBundle\Request\ResolveNextRoute;
+use Sylius\Component\Core\Model\PaymentInterface;
+
+final class ResolveNextRouteAction implements ActionInterface
+{
+    /**
+     * @param ResolveNextRoute $request
+     */
+    public function execute($request): void
+    {
+        /** @var PaymentInterface $payment */
+        $payment = $request->getFirstModel();
+
+        if (
+            $payment->getState() === PaymentInterface::STATE_COMPLETED ||
+            $payment->getState() === PaymentInterface::STATE_AUTHORIZED
+        ) {
+            $request->setRouteName('sylius_shop_order_thank_you');
+
+            return;
+        }
+
+        $request->setRouteName('sylius_shop_checkout_complete');
+    }
+
+    public function supports($request): bool
+    {
+        return
+            $request instanceof ResolveNextRoute &&
+            $request->getFirstModel() instanceof PaymentInterface
+        ;
+    }
+}
